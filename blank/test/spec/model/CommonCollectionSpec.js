5593c8bb0043bfe5158b08fd8a8f52cd3a421881
nishinaka shinya <n-shinya@flect.co.jp>
2013-06-16 15:20:44 +0900

    refs #21 CommonCollection#query,findAllのテストを追加しました。

--- blank/test/spec/model/CommonCollectionSpec.js
+++ blank/test/spec/model/CommonCollectionSpec.js
@@ -1,4 +1,4 @@
-define(['model/CommonCollection', 'db', 'sinon', 'forcetk-extend'], function(CommonCollection, db, sinon, forcetk) {
+define(['model/CommonCollection', 'model/CommonModel', 'db', 'sinon', 'forcetk-extend'], function(CommonCollection, CommonModel, db, sinon, forcetk) {
 
   return describe("CommonCollection.jsのテスト", function() {
 
@@ -10,6 +10,8 @@ define(['model/CommonCollection', 'db', 'sinon', 'forcetk-extend'], function(Com
         function(tx) {
           tx.executeSql("DROP TABLE IF EXISTS HELLO");
           tx.executeSql("CREATE TABLE HELLO(id primary key, name, sync_status)");
+          tx.executeSql("INSERT INTO HELLO (id, name, sync_status) VALUES ('001', 'name001', '0')");
+          tx.executeSql("INSERT INTO HELLO (id, name, sync_status) VALUES ('002', 'name002', '0')");
         },
         function(err) {
           throw new Error("データベースの初期化に失敗しました。");
@@ -32,6 +34,14 @@ define(['model/CommonCollection', 'db', 'sinon', 'forcetk-extend'], function(Com
         // forcetkの初期化
         forcetk.setSessionToken("dummySessionId", null, "/dummyInstanceURL");
       });
+    });
+
+    afterEach(function() {
+      db.getConn().transaction(
+        function(tx) {
+          tx.executeSql("DROP TABLE IF EXISTS HELLO");
+        }
+      );
     });
 
     // TEST FOR CommonCollection#fetch
@@ -138,6 +148,141 @@ define(['model/CommonCollection', 'db', 'sinon', 'forcetk-extend'], function(Com
         runs(function() {
           expect(calledCallback).toBeTruthy();
           expect(collection.length).toEqual(0);
+        });
+      });
+    });
+
+    // TEST FOR CommonCollection#query
+    describe("データベースへの検索", function() {
+      it("データ取得に成功して1件以上の結果が返却されるとadd:allイベントが発生して内包するモデルにデータがバインドされる", function() {
+        var calledCallback = false;
+
+        var collection = new CommonCollection();
+        collection.bind("add:all", function() {
+          calledCallback = true;
+        });
+        collection.query("SELECT * FROM HELLO ORDER BY id", []);
+
+        waitsFor(function() {
+          return calledCallback;
+        }, "", 5000);
+
+        runs(function() {
+          expect(calledCallback).toBeTruthy();
+          expect(collection.length).toEqual(2);
+          var model1 = collection.at(0);
+          expect(model1.id).toEqual("001");
+          expect(model1.get("name")).toEqual("name001");
+          var model2 = collection.at(1);
+          expect(model2.id).toEqual("002");
+          expect(model2.get("name")).toEqual("name002");
+        });
+      });
+
+      it("データ取得に成功して結果が0件の場合はnotfoundイベントが発生する", function() {
+        var calledCallback = false;
+
+        var collection = new CommonCollection();
+        collection.bind("notfound", function() {
+          calledCallback = true;
+        });
+        collection.query("SELECT * FROM HELLO WHERE id = ? ORDER BY id", ["NOEXIST"]);
+
+        waitsFor(function() {
+          return calledCallback;
+        }, "", 5000);
+
+        runs(function() {
+          expect(calledCallback).toBeTruthy();
+          expect(collection.length).toEqual(0);
+        });
+      });
+    });
+
+    // TEST FOR CommonCollection#findAll
+    describe("データベースへの全件検索", function() {
+      it("tableNameが設定されていない場合は例外が送出される", function() {
+        var collection = new CommonCollection();
+        try {
+          collection.findAll();
+        } catch (e) {
+          expect(e).not.toBeUndefined();
+        }
+      });
+
+      it("tableNameが設定されていない場合は例外が送出される", function() {
+        var collection = new CommonCollection();
+        try {
+          collection.findAll();
+        } catch (e) {
+          expect(e).not.toBeUndefined();
+        }
+      });
+
+      it("データ取得に成功して1件以上の結果が返却されるとadd:allイベントが発生して内包するモデルにデータがバインドされる", function() {
+        var calledCallback = false;
+
+        var collection = new CommonCollection();
+        collection.tableName = "HELLO";
+        collection.bind("add:all", function() {
+          calledCallback = true;
+        });
+        collection.findAll();
+
+        waitsFor(function() {
+          return calledCallback;
+        }, "", 5000);
+
+        runs(function() {
+          expect(calledCallback).toBeTruthy();
+          expect(collection.length).toEqual(2);
+          var model1 = collection.at(0);
+          expect(model1.id).toEqual("001");
+          expect(model1.get("name")).toEqual("name001");
+          var model2 = collection.at(1);
+          expect(model2.id).toEqual("002");
+          expect(model2.get("name")).toEqual("name002");
+        });
+      });
+
+      it("データ取得に成功して結果が0件の場合はnotfoundイベントが発生する", function() {
+        var initialized = false;
+        db.getConn().transaction(
+          function(tx) {
+            // 全件削除
+            tx.executeSql("DELETE FROM HELLO");
+          },
+          function(err) {
+            throw new Error("削除に失敗");
+          },
+          function() {
+              initialized = true;
+          }
+        );
+
+        waitsFor(function() {
+          return initialized;
+        }, "", 5000);
+
+        runs(function() {
+
+          var calledCallback = false;
+
+          var collection = new CommonCollection();
+          collection.tableName = "HELLO";
+          collection.bind("notfound", function() {
+            calledCallback = true;
+          });
+          collection.findAll();
+
+          waitsFor(function() {
+            return calledCallback;
+          }, "", 5000);
+
+          runs(function() {
+            expect(calledCallback).toBeTruthy();
+            expect(collection.length).toEqual(0);
+          });
         });
       });
     });
