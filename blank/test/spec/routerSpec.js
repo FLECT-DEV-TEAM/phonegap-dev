d88a1c61bd54caab9c45f005bd645ebc46a7b817
nishinaka shinya <n-shinya@flect.co.jp>
2013-06-17 21:26:59 +0900

    refs #22 router.jsのテストを実装しました。
    ・CommonRouter#viewにビュー名を指定するようにしました。

--- blank/test/spec/routerSpec.js
+++ blank/test/spec/routerSpec.js
@@ -1,12 +1,60 @@
-define(['router', 'view/HelloView'], function(router, HelloView) {
+define(['router', 'view/HelloView', 'view/CommonView'], function(router, HelloView, CommonView) {
 
     return describe("router.jsのテスト", function() {
 
         describe("CommonRouterのviewのテスト", function() {
-            // CommonRouter#view
-            it("キャッシュにViewインスタンスがない場合はViewを新たに初期化してキャッシュに格納する", function() {
+
+            var handlebarsStub;
+
+            beforeEach(function() {
+                handlebarsStub = sinon.stub(Handlebars, "compile");
+                handlebarsStub.returns(function(){});
             });
-            it("キャッシュにViewインスタンスがある場合はViewを再利用する", function() {
+
+            afterEach(function() {
+                handlebarsStub.restore();
+            });
+
+            // CommonRouter#view
+            it("Viewインスタンスが指定されていない場合は例外を送出する", function() {
+                try {
+                    router.view(undefined, "Hello");
+                } catch (e) {
+                    expect(e).not.toBeUndefined();
+                }
+            });
+
+            it("View名が指定されていない場合は例外を送出する", function() {
+                try {
+                    router.view(HelloView, undefined);
+                } catch (e) {
+                    expect(e).not.toBeUndefined();
+                }
+            });
+
+            it("キャッシュにViewインスタンスがない場合はViewを新たに初期化してキャッシュに格納する", function() {
+                var cache = router._cache.view.Hello;
+                expect(cache).toBeUndefined();
+
+                router.view(HelloView, "Hello");
+
+                var cached = router._cache.view.Hello;
+                expect(cached).not.toBeUndefined();
+
+            });
+
+            it("キャッシュにViewインスタンスがある場合は再利用する", function() {
+                router._cache.view.Hello = undefined;
+
+                router.view(HelloView, "Hello");
+                var cached1 = router._cache.view.Hello;
+
+                router.view(HelloView, "Hello");
+                var cached2 = router._cache.view.Hello;
+
+                var sameInstance = cached1 === cached2;
+                expect(sameInstance).toBeTruthy();
+
             });
         });
     });
--- blank/www/js/router.js
+++ blank/www/js/router.js
@@ -1,36 +1,51 @@
 define(['backbone', 'view/HelloView'], function(Backbone, HelloView) {
 
-    // Application Common Router.
+    /** 
+    * Backbone.Routerを拡張した共通モデルです。
+    * 
+    * @class CommonRouter
+    */
     var common = Backbone.Router.extend({
 
         _cache : {
             view : {}
         },
 
-        view: function(viewInstance, params) {
-            var cached = this._cache.view.viewInstance;
+        /** 
+         * Backboneのビューを初期化します。
+         *         
+         * @param {Object} viewObject ビューオブジェクト
+         * @param {String} viewName ビュー名。キャッシュのキーになるため、アプリケーション内で一意になるようにします。
+         * @param {Object} params ビューのinitializeに渡すパラメータ (option)
+         * @throws {Error} ビューオブジェクトが指定されていない場合
+         * @throws {Error} ビュー名が指定されていない場合
+         */
+        view: function(viewObject, viewName, params) {
+            if (!viewObject) {
+                throw new Error("ビューオブジェクトが指定されていません");
+            }
+            if (!viewName) {
+                throw new Error("ビュー名が指定されていません");
+            }
+            var cached = this._cache.view[viewName];
             if (cached) {
                 cached.initialize(params);
             } else {
-                this._cache.view.viewInstance = new viewInstance(params);
+                this._cache.view[viewName] = new viewObject(params);
             }
         }
     });
 
-    router = {
+    var Router = common.extend({
+        routes: {
+            "hello": "hello"
+        },
 
-        Router: common.extend({
+        hello: function() {
+            this.view(HelloView, "Hello");
+        }
+    });
 
-            routes: {
-                "hello": "hello"
-            },
-
-            hello: function() {
-                this.view(HelloView);
-            }
-
-        })
-    };
-    return new router.Router();
+    return new Router();
 
 });
