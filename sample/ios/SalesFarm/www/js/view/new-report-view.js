053f4469a1c16e7b02ddfc1b5c428b9c49e44079
nishinaka shinya <n-shinya@flect.co.jp>
2013-07-09 23:38:34 +0900

    感情アイコンを押したときの処理を追加しました。

--- sample/ios/SalesFarm/www/js/view/new-report-view.js
+++ sample/ios/SalesFarm/www/js/view/new-report-view.js
@@ -4,8 +4,6 @@ function(CommonView, $) {
 
     return CommonView.extend({
 
-        el: "#container",
-
         initialize: function() {
             this.render();
         },
@@ -17,6 +15,44 @@ function(CommonView, $) {
             $(".title").html("新しい登録");
             // contents
             $(this.template('#new-report-template')).slide();
+            this.event();
+        },
+
+        event: function() {
+
+            $(".icon-camera").on("touchstart", this.touchCameraIcon);
+
+            var laughIcon = $(".icon-emo-laugh");
+            var happyIcon = $(".icon-emo-happy");
+            var unhappyIcon = $(".icon-emo-unhappy");
+            var iconDefaultColor = "#666";
+
+            var form = $("#new-report");
+
+            laughIcon.on("touchstart", function() {
+                $(this).css("color", "#d00");
+                happyIcon.css("color", iconDefaultColor);
+                unhappyIcon.css("color", iconDefaultColor);
+                form.data("emotion", "laugh");
+            });
+
+            happyIcon.on("touchstart", function() {
+                $(this).css("color", "#fa0");
+                laughIcon.css("color", iconDefaultColor);
+                unhappyIcon.css("color", iconDefaultColor);
+                form.data("emotion", "happy");
+            });
+
+            unhappyIcon.on("touchstart", function() {
+                $(this).css("color", "#0ff");
+                laughIcon.css("color", iconDefaultColor);
+                happyIcon.css("color", iconDefaultColor);
+                form.data("emotion", "unhappy");
+            });
+        },
+
+        touchCameraIcon: function() {
+            alert("カメラを起動");
         }
     });
 
