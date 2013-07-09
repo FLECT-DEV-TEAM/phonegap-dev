4fd624553a48d3b3b69b65abf6c295dbe222d5b2
n-shinya <n-shinya@flect.co.jp>
2013-07-09 19:27:12 +0900

    登録画面のひな形を修正しました。

--- sample/ios/SalesFarm/www/js/view/new-report-view.js
+++ sample/ios/SalesFarm/www/js/view/new-report-view.js
@@ -4,6 +4,8 @@ function(CommonView, $) {
 
     return CommonView.extend({
 
+        el: "#container",
+
         initialize: function() {
             this.render();
         },
@@ -12,8 +14,7 @@ function(CommonView, $) {
             // header            
             var header = this.template("#new-report-header-template");
             $(".header").empty().append(header);
-            $(".title").html("");
-
+            $(".title").html("新しい登録");
             // contents
             $(this.template('#new-report-template')).slide();
         }
